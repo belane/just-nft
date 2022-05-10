905e79d1a8daef46f3677167ab749dbc365f199e
Belane <srbelane@gmail.com>
2022-05-10 13:22:55 +0200

    project

---
+++ hardhat.config.js
@@ -0,0 +1,30 @@
+require("@nomiclabs/hardhat-waffle");
+require("hardhat-gas-reporter");
+require("solidity-coverage");
+require("dotenv").config();
+require("hardhat-tracer");
+
+const COINAPI = process.env.COINAPI;
+
+module.exports = {
+  solidity: {
+    compilers: [
+      {
+        version: '0.8.13',
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 500,
+          },
+        },
+      },
+    ],
+  },
+  gasReporter: {
+    enabled: true,
+    currency: "USD",
+    // gasPriceApi: "https://api.snowtrace.io/api?module=proxy&action=eth_gasPrice",
+    // token: "AVAX",
+    coinmarketcap: COINAPI,
+  },
+};
